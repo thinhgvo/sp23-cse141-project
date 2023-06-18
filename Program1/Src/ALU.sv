a94e3d556a94af76b3cc56e39fb7f3dd629397fb
Thinh Vo <75603309+thinhgvo@users.noreply.github.com>
2023-06-18 03:36:05 -0700

    ALU works correctly now

--- Program1/Src/ALU.sv
+++ Program1/Src/ALU.sv
@@ -11,36 +11,46 @@ module ALU #(parameter W=8) (
     input [W-1:0] ReadA,
                   ReadB,
     input [4:0]   Immediate,
-    input [3:0]   Operand,
+    input [3:0]   ROperand,
+	input [2:0]	  IOperand,
+	input 		  OpType,
     output logic [W-1:0] Output,
     output logic Zero,
-    output wire Equal
+    output wire  Equal
     );
 
     assign Equal = (ReadA == ReadB) ? 1 : 0;
 
     always_comb begin 
-
-        case (Operand)
-            AND  : Output = ReadA & ReadB;
-            OR   : Output = ReadA | ReadB;
-            NOT  : Output = ~ReadA | ReadB;
-            SUB  : Output = ReadA - ReadB;
-            STR  : Output = ReadB;
-            SLT  : Output = (ReadB < ReadA) ? ReadB : (ReadA < ReadB) ? ReadA : 0;
-            SEQ  : Output = (ReadA == ReadB) ? ReadA : 0;
-            MVTO : Output = ReadB;
-            // MVFR : Output = InputA + InputB;
-            XOR  : Output = ReadA ^ ReadB;
-            XORR : Output = ^ReadB;
-            ADDI : Output = ReadA + Immediate;
-            SUBI : Output = ReadA - Immediate;
-            LSRI : Output = ReadA >> Immediate;
-            LSLI : Output = ReadA << Immediate;
-            LUT  : Output = Immediate;
-            default: Output = 0;
+		  case (OpType)
+           0 : case (ROperand)
+                AND  : Output = ReadA & ReadB;
+                OR   : Output = ReadA | ReadB;
+                ADD  : Output = ReadA + ReadB;
+                NOT  : Output = ~ReadA;
+                // NOT  : Output = ReadA + 1;
+                SUB  : Output = ReadA - ReadB;
+                STR  : Output = ReadA;
+				LOAD : Output = ReadA;
+				SLT  : Output = ReadA < ReadB;
+				SEQ  : Output = ReadA == ReadB;
+                MVTO : Output = ReadA;
+                BTRU : Output = (ReadA == 1) ? 1 : 0;
+                // MVFR : Output = InputA + InputB;
+                XOR  : Output = ReadA ^ ReadB;
+                XORR : Output = ^ReadB;
+                default: Output = 0;
+           endcase
+			  1 : case (IOperand)
+                ADDI : Output = ReadA + Immediate;
+                SUBI : Output = ReadA - Immediate;
+                LSRI : Output = ReadA >> Immediate;
+                LSLI : Output = ReadA << Immediate;
+                B    : Output = (Immediate == 1) ? 1 : 0;
+                LUT  : Output = Immediate;
+                default: Output = 0;
+           endcase
         endcase
-
         case (Output)
             8'b0 : Zero = 1; 
             default: Zero = 0;
--- Program1/Src/ALU_tb.sv
+++ Program1/Src/ALU_tb.sv
@@ -20,28 +20,33 @@
 // 
 ////////////////////////////////////////////////////////////////////////////////
 import Operations::*;
-module ALU_tb;
+
+module ALU_tb();
 
 // Inputs
-  logic [ 3:0] Operand;
+  logic [ 3:0] ROperand;
+  logic [ 2:0] IOperand;
   logic [ 7:0] ReadA;
   logic [ 7:0] ReadB;
   logic [ 4:0] Immediate;
+  logic 			OpType;
 
 // Outputs
   wire [ 7:0] Output;
-  wire        Zero;
+  logic       Zero;
   wire        Equal;
 
 	// Instantiate the Unit Under Test (UUT)
   ALU #(8) uut (
-		.Operand,
-		.ReadA, 
-		.ReadB,
-    .Immediate, 
-		.Output, 
-		.Zero, 
-		.Equal
+		.ROperand(ROperand),
+		.IOperand(IOperand),
+		.ReadA(ReadA), 
+		.ReadB(ReadB),
+		.OpType(OpType),
+		.Immediate(Immediate), 
+		.Output(Output), 
+		.Zero(Zero), 
+		.Equal(Equal)
 	);
 
 initial begin
@@ -49,15 +54,169 @@ initial begin
   #100ns;
         
 // Add stimulus here
-  ReadA = 8'h0004;
-  ReadB = 8'h0004;
-  #20ns  Operand = SUB;
-  $display("sub Output = 0? %d", Output);
-  #20ns	 Operand = AND;
-  $display("and Output = 4? %d", Output);
-  #20ns	 ReadB = 8'h0003;
-  Operand = XOR;
-  $display("xor Output = 7? %d", Output);
+  ReadA = 8'b0000_0100;
+  ReadB = 8'b0000_0100;
+  
+  // ADD Test:
+  #20ns;
+  OpType    = 0;
+  #20ns;
+  ROperand  = ADD;
+  #1ns;
+  if (Output == 8'b0000_1000) begin
+    $display("PASSED ADD Test: {Expected: 8, Received: %d}", Output);
+  end
+
+  // SUB Test:
+  #20ns;
+  ROperand  = SUB;
+  #1ns;
+  if (Output == 8'b0000_0000) begin
+    $display("PASSED SUB Test: {Expected: 0, Received = %d}", Output);
+  end
+
+  // AND Test:
+  #20ns;
+  ROperand  = AND;
+  #1ns;
+  if (Output == 8'b0000_0100) begin
+	 $display("PASSED AND Test: {Expected: 4, Received: %d}", Output);
+  end
+
+  // OR Test:
+  #20ns;
+  ROperand  = OR;
+  #1ns;
+  if (Output == 8'b0000_0100) begin
+    $display("PASSED OR Test: {Expected: 4, Received: %d}", Output);
+  end
+
+  // STR Test:
+  #20ns;
+  ROperand  = STR;
+  #1ns;
+  if (Output == 8'b0000_0100) begin
+    $display("PASSED STR Test: {Expected: 4, Received: %d}", Output);
+  end
+
+  // LOAD Test:
+  #20ns;
+  ROperand  = LOAD;
+  #1ns;
+  if (Output == 8'b0000_0100) begin
+    $display("PASSED LOAD Test: {Expected: 4, Received: %d}", Output);
+  end
+
+  // SLT Test:
+  #20ns;
+  ReadB     = 8'b0000_0011;
+  ROperand  = SLT;
+  #1ns;
+  if (Output == 8'b0000_0000) begin
+    $display("PASSED SLT Test: {Expected: 0, Received: %d}", Output);
+  end
+
+  // XORR Test:
+  #20ns;
+  ROperand  = XORR;
+  #1ns;
+  if (Output == 8'b0000_0000) begin
+    $display("PASSED XORR Test: {Expected: 0, Received: %d}", Output);
+  end
+
+  // XOR Test:
+  #20ns;
+  ROperand  = XOR;
+  #1ns;
+  if (Output == 8'b0000_0111) begin
+    $display("PASSED XOR Test: {Expected: 7, Received: %d}", Output);
+  end
+
+  // SEQ Test:
+  #20ns;
+  ReadA     = 8'b0000_0011;
+  ROperand  = SEQ;
+  #1ns;
+  if (Output == 8'b0000_0001) begin
+    $display("PASSED SEQ Test: {Expected: 1, Received: %d}", Output);
+  end
+
+  // BTRU Test:
+  #20ns;
+  ReadA     = 8'b0000_0001;
+  ROperand  = BTRU;
+  #1ns;
+  if (Output == 8'b0000_0001) begin
+    $display("PASSED BTRU Test: {Expected: 1, Received: %d}", Output);
+  end
+
+  // NOT Test:
+  #20ns;
+  ROperand  = NOT;
+  #1ns;
+  if (Output == 8'b1111_1110) begin
+    $display("PASSED NOT Test: {Expected: 254, Received: %d}", Output);
+  end
+
+  // ADDI Test:
+  #20ns;
+  OpType    = 1;
+  #1ns;
+  ReadA     = 8'b0000_0001;
+  Immediate = 5;
+  IOperand  = ADDI;
+  #1ns;
+  if (Output == 8'b0000_0110) begin
+    $display("PASSED ADDI Test: {Expected: 6, Received: %d}", Output);
+  end
+
+  // SUBI Test:
+  #20ns;
+  ReadA     = 8'b0000_0111;
+  Immediate = 1;
+  IOperand  = SUBI;
+  #1ns;
+  if (Output == 8'b0000_0110) begin
+    $display("PASSED SUBI Test: {Expected: 6, Received: %d}", Output);
+  end
+
+  // LSLI Test:
+  #20ns;
+  ReadA     = 8'b0000_0100;
+  Immediate = 2;
+  IOperand  = LSLI;
+  #1ns;
+  if (Output == 8'b0001_0000) begin
+    $display("PASSED LSLI Test: {Expected: 16, Received: %d}", Output);
+  end
+
+  // LSRI Test:
+  #20ns;
+  ReadA     = 8'b0000_0100;
+  Immediate = 2;
+  IOperand  = LSRI;
+  #1ns;
+  if (Output == 8'b0000_0001) begin
+    $display("PASSED LSRI Test: {Expected: 1, Received: %d}", Output);
+  end
+
+  // LUT Test:
+  #20ns;
+  Immediate = 10;
+  IOperand  = LUT;
+  #1ns;
+  if (Output == 8'b0000_1010) begin
+    $display("PASSED LUT Test: {Expected: 10, Received: %d}", Output);
+  end
+
+  // B Test:
+  #20ns;
+  Immediate = 1;
+  IOperand  = B;
+  #1ns;
+  if (Output == 8'b0000_0001) begin
+    $display("PASSED B Test: {Expected: 1, Received: %d}", Output);
+  end
 end
 
 endmodule
